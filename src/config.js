69874050302146b1d87590144a2816bde68e0de0
krogank9 <krogank9@github.com>
2021-08-07 05:55:20 +0000

    change config for prod

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	JWT_SECRET: process.env.JWT_SECRET,
 	JWT_EXPIRY: 60*60,
-	CLIENT_ORIGIN: !process.env.NODE_ENV ? "http://localhost:3000" : "https://cryptodash.ltkdigital.com"
+	CLIENT_ORIGIN: process.env.NODE_ENV === 'production' ? "http://cryptodash.ltkdigital.com" : "http://localhost:3000" 
 }
